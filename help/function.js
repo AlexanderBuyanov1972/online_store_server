df2335593d486ab79f676c289a1952412f50e125
AlexanderBuyanov <globalist72@gmail.com>
2021-11-05 15:24:32 +0200

    2021-11-05

--- help/function.js
+++ help/function.js
@@ -7,5 +7,23 @@ class HelpFunction {
             ...array.filter(item => item.name !== elementName).sort(compare)
         ]
     }
+
+    async getArraydeviceCount(arrayDevices) {
+        const map = new Map()
+        for (let i = 0; i < arrayDevices.length; i++) {
+            if (!map.has(arrayDevices[i].deviceId)) {
+                map.set(arrayDevices[i].deviceId, 1)
+            } else {
+                map.set(arrayDevices[i].deviceId, map.get(arrayDevices[i].deviceId) + 1)
+            }
+        }
+        let result = []
+        for (let key of map.keys()) {
+            let dev = await Device.findOne({ where: { id: key } })
+            result.push({ device: dev, count: map.get(key) })
+        }
+        return result
+    }
 }
+
 export default new HelpFunction
